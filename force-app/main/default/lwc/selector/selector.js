c2466c6734a97c418401254df444957a557ca0fc
KartheekL <kartheekl228@gmail.com>
2020-11-20 02:00:47 +0530

    added wire functionality

--- force-app/main/default/lwc/selector/selector.js
+++ force-app/main/default/lwc/selector/selector.js
@@ -1,9 +1,22 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import {getRecord, getFieldValue} from 'lightning/uiRecordApi';
+import Id from '@salesforce/user/Id';
+import NAME_FIELD from '@salesforce/schema/User.Name';
+
 
 export default class Selector extends LightningElement {
     selectedProductId;
 
     handleProductSelected(evt) {
         this.selectedProductId = evt.detail;
-    }
+	}
+
+	recordId = Id;
+	
+	@wire(getRecord, { recordId: '$recordId', fields: [ NAME_FIELD ] } )
+	user;
+
+	get name(){
+		return getFieldValue(this.user.data,NAME_FIELD);
+	}
 }
