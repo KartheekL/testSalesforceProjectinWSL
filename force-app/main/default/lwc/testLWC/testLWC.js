5bac5c24056682de1dc43fa800c17d8d9366b783
KartheekL <kartheekl228@gmail.com>
2020-11-19 22:45:05 +0530

    commiting all changes

--- force-app/main/default/lwc/testLWC/testLWC.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class TestLWC extends LightningElement {
-	data;
-	output;
-	updateOutput(event){
-		this.output = event.target.value;
-	}
-}
